6cb6a125f48a3d240da1ad1be448902ac50fe1a7
linwei <1194687762@qq.com>
2019-10-25 21:08:04 +0800

    add customer annotation

--- src/main/java/com/example/demo/controller/RestControllerDemo.java
+++ src/main/java/com/example/demo/controller/RestControllerDemo.java
@@ -1,16 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.annotation.AnalysisActuator;
+
 @RestController
 public class RestControllerDemo {
 
 	@RequestMapping(value = { "/", "hi", "hello" }, method = RequestMethod.GET)
-	public String hello() {
-		return "hello";
+	@AnalysisActuator
+	public ResponseEntity<String> hello() {
+		return new ResponseEntity<String>("hello",HttpStatus.OK);
 	}
 
 	@RequestMapping("/hello/{name}/{age}")
